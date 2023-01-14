bceabfedab7822bf0b2704e19745606dcdf93777
ademarrus <119788923+ademarrus@users.noreply.github.com>
2023-01-14 22:18:28 +0300

    Update api.py

--- api.py
+++ api.py
@@ -71,6 +71,31 @@ class PetFriends:
             result = res.text
         print(result)
         return status, result
+    
+ # добавление питоемца без фото
+
+     def add_new_pet_without_photo(self, auth_key: json, name: str, animal_type: str,
+                    age: str) -> json:
+        """Метод отправляет (постит) на сервер данные о добавляемом питомце и возвращает статус
+        запроса на сервер и результат в формате JSON с данными добавленного питомца"""
+
+        data = MultipartEncoder(
+            fields={
+                'name': name,
+                'animal_type': animal_type,
+                'age': age
+                        })
+        headers = {'auth_key': auth_key['key'], 'Content-Type': data.content_type}
+
+        res = requests.post(self.base_url + 'api/create_pet_simple', headers=headers, data=data)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        print(result)
+        return status, result
 
  # удаление питомца   
     def delete_pet(self, auth_key: json, pet_id: str) -> json:
@@ -110,3 +135,4 @@ class PetFriends:
         except json.decoder.JSONDecodeError:
             result = res.text
         return status, result
+    
